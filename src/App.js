4fd808d233e4450ec431b32363855771438bc46c
Ross Kiser <kiserr10@gmail.com>
2017-11-29 18:18:06 -0500

    first login reducer wired up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-	
+
 	componentWillMount(){
 		// Initialize Firebase
 		const config = {
@@ -23,11 +24,7 @@ class App extends Component {
 	render(){
 		return(
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
-				</View>
+				<LoginForm />
 			</Provider>
 		);
 	}
