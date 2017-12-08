42bbbd22ab06ff21bbcc922cab2a96c5f127439f
Ross Kiser <kiserr10@gmail.com>
2017-12-08 15:55:24 -0700

    spinner in place on login with error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 
@@ -16,6 +17,29 @@ class LoginForm extends Component {
 	onButtonPress(){
 		const { email, password } = this.props;
 		this.props.loginUser({ email, password });
+	}
+
+	renderError(){
+		if(this.props.error){
+			return(
+				<View style={{ backgroundColor: 'white' }}>
+					<Text style={styles.errorTextStyle }>
+						{this.props.error}
+					</Text>
+				</View>
+			);
+		}
+	}
+
+	renderButton(){
+		if (this.props.loading){
+			return <Spinner />;
+		}
+		return(
+			<Button onPress={this.onButtonPress.bind(this)} >
+				Login
+			</Button>
+		);
 	}
 
 	render(){
@@ -38,20 +62,30 @@ class LoginForm extends Component {
 						value={this.props.password}
 					/>
 				</CardSection>
+
+				{this.renderError()}
+
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 			</Card>
 		);
 	}
 }
-const mapStateToProps = state => {
-	return {
-		email: state.auth.email,
-		password: state.auth.password
-	};
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
+const mapStateToProps = ({ auth }) => {
+	const { email, password, error, loading } = auth;
+
+	return { email, password, error, loading };
+};
+
+export default connect(mapStateToProps, {
+	emailChanged, passwordChanged, loginUser,
+})(LoginForm);
