9d8a8b4bead285285d1958c4bc3332701feee3fb
Ross Kiser <kiserr10@gmail.com>
2017-12-12 16:03:24 -0700

    added employee list scene

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 
 
@@ -11,6 +12,7 @@ const RouterComponent = () => {
 	return (
 		<Router sceneStyle={{ paddingTop: 65 }}>
 			<Scene key="login" component={LoginForm} title="Please Login" />
+			<Scene key="employeeList" component={EmployeeList} title="Employees" />
 		</Router>
 	);
 };
