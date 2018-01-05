70745fdf889d231eebfe8d000d7c4207571a0379
Ross Kiser <kiserr10@gmail.com>
2018-01-04 19:05:43 -0700

    added delete page boiler

---
+++ src/components/EmployeeDelete.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DeleteEmployee extends Component {
+	render(){
+		return(
+			<View>
+				<Text>Delete Page</Text>
+			</View>
+		);
+	}
+}
+
+export default DeleteEmployee;
